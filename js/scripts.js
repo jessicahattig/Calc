9243cf42db50cde55012d2f36c29f981ae5216e9
jessicahattig <jessicahattig@gmail.com>
2023-06-07 19:16:05 -0700

    Have html and js code.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// business logic
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+// user interface logic 
+const number1 = prompt("Enter a number:");
+const number2 = prompt("Enter another number:");
+
+window.alert(add(number1, number2));
